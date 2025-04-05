3f0956a1d651ae8d90af82c28ce1601f267cd963
maxcolliander <max.colliander@gmail.com>
2025-04-06 00:35:45 +0200

    Introduced the option to do --save to save the ascii to .avi

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,43 @@
 #include <iostream>     // For input/output
 #include <opencv2/opencv.hpp>  // OpenCV main header for video processing
 #include "../include/ascii_filter.hpp"
+#include <cstdlib>
 
 using namespace std;
 
 // g++ -O2 -o video_player src/main.cpp src/ascii_filter.cpp `pkg-config --cflags --libs opencv4` -Iinclude`
 
+bool isDisplayAvailable(){
+    const char* display = getenv("DISPLAY");
+    return display != nullptr && string(display).length() > 0;
+}
+
+void showProgress(int currentFrame, int totalFrames) {
+    int barWidth = 50;  // Width of the progress bar
+    float progress = (float)currentFrame / totalFrames;
+    int pos = barWidth * progress;
+
+    cout << "[";
+    for (int i = 0; i < barWidth; ++i) {
+        if (i < pos) cout << "#";
+        else cout << " ";
+    }
+    cout << "] " << int(progress * 100.0) << "%\r";  // Display percentage
+    cout.flush();
+}
+
 int main(int argc, char** argv) {
-    if (argc != 2) {
-        cerr << "Usage: " << argv[0] << " <video_file>" << std::endl;
+    if (argc < 2 || argc > 3) {
+        cerr << "Usage: " << argv[0] << " <video_file> [--save]" << std::endl;
         return -1;
     }
+
     string videoFile = argv[1];
-    cv::VideoCapture cap(videoFile);  // Open the video file
+    bool saveToFile = false;
+    if (argc == 3 && string(argv[2]) == "--save") {
+        saveToFile = true;
+    }
+    cv::VideoCapture cap(videoFile);
 
     if (!cap.isOpened()) {
         cerr << "Error: Could not open video file." << std::endl;
@@ -20,7 +45,32 @@ int main(int argc, char** argv) {
     }
     
     double fps = cap.get(cv::CAP_PROP_FPS);
+    int width = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_WIDTH));
+    int height = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_HEIGHT));
+    int totalFrames = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_COUNT));
     cout << "FPS: " << fps << endl;
+
+    cv::VideoWriter writer;
+
+    if (saveToFile) {
+        size_t lastSlash = videoFile.find_last_of('/');
+        size_t lastDot = videoFile.find_last_of('.');
+
+        if (lastDot == std::string::npos || lastDot <= lastSlash) {
+            cerr << "Error: Invalid video file name." << endl;
+            return -1;
+        }
+
+        string baseName = videoFile.substr(lastSlash + 1, lastDot - lastSlash - 1);
+        string outputFile = baseName + "_ascii.avi";
+        writer.open(outputFile, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), fps, cv::Size(width, height), true);
+        if (!writer.isOpened()) {
+            cerr << "Error: Could not open output video file for writing." << endl;
+            return -1;
+        }
+        cout << "Saving to " << outputFile << endl;
+    }
+
 
     cv::Mat frame;
 
@@ -42,17 +92,23 @@ int main(int argc, char** argv) {
         frameCount++;
         double duration = (cv::getTickCount() - startTime) / cv::getTickFrequency();
         totalTime += duration;
-        if (frameCount % 10 == 0) {
-            frames = frameCount / totalTime;
-            cout << "FPS: " << frames << endl;
-        }
-        
-        cv::imshow("ASCII Video", asciiFrame);
-        if (cv::waitKey(1000 / fps) >= 0) {
-            break;
+
+        if (saveToFile) {
+            writer.write(asciiFrame);
+            showProgress(frameCount, totalFrames);
+        } else {
+            if (frameCount % 10 == 0) {
+                frames = frameCount / totalTime;
+                cout << "FPS: " << frames << endl;
+            }
+            cv::imshow("ASCII Video", asciiFrame);
+            if (cv::waitKey(1000 / fps) >= 0) {
+                break;
+            }
         }
 
     }
     cap.release();
+    writer.release();
     cv::destroyAllWindows();
 }
